554cacb4a8f9b1ad082b07e568f9c15f427be259
nightsense <nightsense@users.noreply.github.com>
2018-01-03 06:28:54 -0500

    nudge colours

--- transmogrify.fish
+++ transmogrify.fish
@@ -10,21 +10,38 @@ for f in (find /tmp/flat -type f -name '*vim')
   sed -i $f -e  '/./,$!d'
   sed -i $f -e  's/flattened/stellarized/g'
 
-  sed -i $f -e 's/002b36/242836/'
-  sed -i $f -e 's/073642/303440/'
-  sed -i $f -e 's/586e75/616675/'
+  sed -i $f -e 's/002b36/1b1f2b/'
+  sed -i $f -e 's/073642/282c36/'
+  sed -i $f -e 's/586e75/595d6b/'
   sed -i $f -e 's/657b83/757880/'
   sed -i $f -e 's/839496/948e88/'
   sed -i $f -e 's/93a1a1/9e958b/'
   sed -i $f -e 's/eee8d5/ebdbca/'
   sed -i $f -e 's/fdf6e3/ffe9d4/'
 
-  sed -i $f -e 's/dc322f/d45959/'
-  sed -i $f -e 's/cb4b16/c97622/'
-  sed -i $f -e 's/b58900/ab8000/'
-  sed -i $f -e 's/859900/6b9425/'; sed -i $f -e 's/719e07/6b9425/'
-  sed -i $f -e 's/2aa198/3e9488/'
-  sed -i $f -e 's/268bd2/4c8fb5/'
+end
+
+for f in (find /tmp/flat -type f -name '*dark.vim')
+
+  sed -i $f -e 's/dc322f/ab5555/'
+  sed -i $f -e 's/cb4b16/9e6934/'
+  sed -i $f -e 's/b58900/8a7845/'
+  sed -i $f -e 's/859900/648036/'; sed -i $f -e 's/719e07/648036/'
+  sed -i $f -e 's/2aa198/4a8078/'
+  sed -i $f -e 's/268bd2/4a7994/'
+  sed -i $f -e 's/6c71c4/7a72ab/'
+  sed -i $f -e 's/d33682/94637c/'
+
+end
+
+for f in (find /tmp/flat -type f -name '*light.vim')
+
+  sed -i $f -e 's/dc322f/c96565/'
+  sed -i $f -e 's/cb4b16/bf7830/'
+  sed -i $f -e 's/b58900/9e8434/'
+  sed -i $f -e 's/859900/709431/'; sed -i $f -e 's/719e07/709431/'
+  sed -i $f -e 's/2aa198/4a948a/'
+  sed -i $f -e 's/268bd2/568cab/'
   sed -i $f -e 's/6c71c4/8179b5/'
   sed -i $f -e 's/d33682/bf6f97/'
 
