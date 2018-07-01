814f8d43fb8337854d2e6e55a1eae64ea9d96d24
nightsense <nightsense@users.noreply.github.com>
2018-07-01 14:43:56 -0400

    replicate snow adjustments

--- autoload/airline/themes/stellarized.vim
+++ autoload/airline/themes/stellarized.vim
@@ -1,44 +1,44 @@
-let g:airline#themes#stellarized#palette = {}
+let g:airline#themes#snow#palette = {}
 
 if &background ==# 'light'
-  let s:gry0 = [ '#fcf1e4', 255 ]
-  let s:gry1 = [ '#e4dacc', 253 ]
-  let s:gry3 = [ '#666775', 241 ]
-  let s:red_ = [ '#da634d', 203 ]
-  let s:orng = [ '#c47430', 166 ]
-  let s:gren = [ '#5e9738', 70  ]
-  let s:blue = [ '#448ed6', 32  ]
+  let s:gry0 = [ '#e9f5ff', 255 ]
+  let s:gry1 = [ '#dde9f6', 253 ]
+  let s:gry2 = [ '#5b6f84', 241 ]
+  let s:red_ = [ '#e7313e', 203 ]
+  let s:orng = [ '#cb6300', 166 ]
+  let s:gren = [ '#008e00', 70  ]
+  let s:blue = [ '#007cd1', 32  ]
 else
-  let s:gry0 = [ '#282835', 235 ]
-  let s:gry1 = [ '#3a3b47', 237 ]
-  let s:gry3 = [ '#aba194', 247 ]
-  let s:red_ = [ '#b57d70', 167 ]
-  let s:orng = [ '#ab8264', 137 ]
-  let s:gren = [ '#789265', 65  ]
-  let s:blue = [ '#6d8cb5', 67  ]
+  let s:gry0 = [ '#202a38', 235 ]
+  let s:gry1 = [ '#2e3846', 237 ]
+  let s:gry2 = [ '#8693a7', 247 ]
+  let s:red_ = [ '#d07967', 167 ]
+  let s:orng = [ '#bb8d6b', 137 ]
+  let s:gren = [ '#93a27c', 65  ]
+  let s:blue = [ '#87a5ce', 67  ]
 endif
 
-let s:nrm1 = [ s:gry0[0] , s:gry3[0] , s:gry0[1] , s:gry3[1] ]
-let s:nrm2 = [ s:gry3[0] , s:gry1[0] , s:gry3[1] , s:gry1[1] ]
+let s:nrm1 = [ s:gry0[0] , s:gry2[0] , s:gry0[1] , s:gry2[1] ]
+let s:nrm2 = [ s:gry2[0] , s:gry1[0] , s:gry2[1] , s:gry1[1] ]
 let s:insr = [ s:gry0[0] , s:gren[0] , s:gry0[1] , s:gren[1] ]
 let s:visl = [ s:gry0[0] , s:blue[0] , s:gry0[1] , s:blue[1] ]
 let s:rplc = [ s:gry0[0] , s:red_[0] , s:gry0[1] , s:red_[1] ]
-let s:inac = [ s:gry3[0] , s:gry1[0] , s:gry3[1] , s:gry1[1] ]
+let s:inac = [ s:gry2[0] , s:gry1[0] , s:gry2[1] , s:gry1[1] ]
 
-let g:airline#themes#stellarized#palette.normal = 
+let g:airline#themes#snow#palette.normal = 
   \ airline#themes#generate_color_map( s:nrm1 , s:nrm2 , s:nrm2 )
 
-let g:airline#themes#stellarized#palette.insert = 
+let g:airline#themes#snow#palette.insert = 
   \ airline#themes#generate_color_map( s:insr , s:nrm2 , s:nrm2 )
 
-let g:airline#themes#stellarized#palette.visual = 
+let g:airline#themes#snow#palette.visual = 
   \ airline#themes#generate_color_map( s:visl , s:nrm2 , s:nrm2 )
 
-let g:airline#themes#stellarized#palette.replace = 
+let g:airline#themes#snow#palette.replace = 
   \ airline#themes#generate_color_map( s:rplc , s:nrm2 , s:nrm2 )
 
-let g:airline#themes#stellarized#palette.inactive = 
+let g:airline#themes#snow#palette.inactive = 
   \ airline#themes#generate_color_map( s:inac , s:inac , s:inac )
 
-let g:airline#themes#stellarized#palette.ctrlp =
+let g:airline#themes#snow#palette.ctrlp =
   \ airline#extensions#ctrlp#generate_color_map( s:nrm2 , s:nrm1 , s:nrm2 )
--- templates/template-generator.py
+++ templates/template-generator.py
@@ -6,72 +6,33 @@ def tohex(l,c,h):
   clamp = sRGBColor(rgb.clamped_rgb_r, rgb.clamped_rgb_g, rgb.clamped_rgb_b)
   return clamp.get_rgb_hex()
 
-def formula1(x):
-  return 0.0618**3*x + 6.83/12.0*100.0
+hexgry0_lt = tohex( 11.50/12.0*100.0 , 1.00/12.0*100 , 075.0 )
+hexgry1_lt = tohex( 11.00/12.0*100.0 , 1.00/12.0*100 , 075.0 )
+hexgry2_lt = tohex( 05.50/12.0*100.0 , 1.75/12.0*100 , 285.5 )
+hexgry3_lt = tohex( 03.75/12.0*100.0 , 1.75/12.0*100 , 285.5 )
 
-def formula2(x):
-  return 0.0618**2*x + 6.83/12.0*100.0
+hexred__lt = tohex( 06.25/12.0*100.0 , 09.5/12.0*100 , 030.0 )
+hexorng_lt = tohex( 06.50/12.0*100.0 , 12.0/12.0*100 , 067.5 )
+hexyllw_lt = tohex( 10.00/12.0*100.0 , 12.0/12.0*100 , 082.5 )
+hexgren_lt = tohex( 06.00/12.0*100.0 , 12.0/12.0*100 , 127.5 )
+hexaqua_lt = tohex( 05.50/12.0*100.0 , 12.0/12.0*100 , 187.5 )
+hexblue_lt = tohex( 05.75/12.0*100.0 , 07.0/12.0*100 , 255.0 )
+hexprpl_lt = tohex( 05.75/12.0*100.0 , 07.0/12.0*100 , 307.5 )
+hexpink_lt = tohex( 06.50/12.0*100.0 , 07.0/12.0*100 , 000.0 )
 
-def formula3(x):
-  return 0.0618**1*x + 4.55/12.0*100.0
+hexgry0_dk = tohex( 02.00/12.0*100.0, 01.25/12.0*100 , 285.0 )
+hexgry1_dk = tohex( 02.75/12.0*100.0, 01.25/12.0*100 , 285.0 )
+hexgry2_dk = tohex( 07.25/12.0*100.0, 01.5/12.0*100 , 075.0 )
+hexgry3_dk = tohex( 09.00/12.0*100.0, 01.5/12.0*100 , 075.0 )
 
-def formula4(x):
-  return 0.0618**2*x + 3.03/12.0*100.0
-
-def lum_lt(h):
-  return formula1(h[0])
-def yum_lt(h):
-  return formula2(h[0])*(17.0/12.0)
-def lum_dk(h):
-  return formula2(h[0])
-def yum_dk(h):
-  return formula1(h[0])*(15.0/12.0)
-
-def cro_lt(h):
-  return formula3(h[1])
-def yro_lt(h):
-  return formula3(h[1])*(17.0/12.0)
-def cro_dk(h):
-  return formula4(h[1])
-def yro_dk(h):
-  return formula4(h[1])*(15.0/12.0)
-
-base_sat = 100.0*1.0/12.0
-
-red_ = [ 277.5, 337.5, 037.5 ]
-orng = [ 255.0, 315.0, 060.0 ]
-yllw = [ 232.5, 292.5, 082.5 ]
-gren = [ 187.5, 247.5, 127.5 ]
-aqua = [ 135.0, 195.0, 180.0 ]
-blue = [ 052.5, 112.5, 262.5 ]
-prpl = [ 000.0, 060.0, 315.0 ]
-pink = [ 300.0, 000.0, 015.0 ]
-
-hexgry0_lt = tohex(11.50/12.0*100.0,base_sat,75.0)
-hexgry1_lt = tohex(10.50/12.0*100.0,base_sat,75.0)
-hexgry2_lt = tohex(06.75/12.0*100.0,base_sat,285.0)
-hexgry3_lt = tohex(05.25/12.0*100.0,base_sat,285.0)
-hexred__lt = tohex(lum_lt(red_),cro_lt(red_),red_[2])
-hexorng_lt = tohex(lum_lt(orng),cro_lt(orng),orng[2])
-hexyllw_lt = tohex(yum_lt(yllw),yro_lt(yllw),yllw[2])
-hexgren_lt = tohex(lum_lt(gren),cro_lt(gren),gren[2])
-hexaqua_lt = tohex(lum_lt(aqua),cro_lt(aqua),aqua[2])
-hexblue_lt = tohex(lum_lt(blue),cro_lt(blue),blue[2])
-hexprpl_lt = tohex(lum_lt(prpl),cro_lt(prpl),prpl[2])
-hexpink_lt = tohex(lum_lt(pink),cro_lt(pink),pink[2])
-
-hexgry0_dk = tohex(02.00/12.0*100.0,base_sat,285.0)
-hexgry1_dk = tohex(03.00/12.0*100.0,base_sat,285.0)
-hexgry2_dk = tohex(06.25/12.0*100.0,base_sat,75.0)
-hexgry3_dk = tohex(08.00/12.0*100.0,base_sat,75.0)
-hexred__dk = tohex(lum_dk(red_),cro_dk(red_),red_[2])
-hexorng_dk = tohex(lum_dk(orng),cro_dk(orng),orng[2])
-hexyllw_dk = tohex(yum_dk(yllw),yro_dk(yllw),yllw[2])
-hexgren_dk = tohex(lum_dk(gren),cro_dk(gren),gren[2])
-hexaqua_dk = tohex(lum_dk(aqua),cro_dk(aqua),aqua[2])
-hexblue_dk = tohex(lum_dk(blue),cro_dk(blue),blue[2])
-hexprpl_dk = tohex(lum_dk(prpl),cro_dk(prpl),prpl[2])
-hexpink_dk = tohex(lum_dk(pink),cro_dk(pink),pink[2])
+hexred__dk = tohex( 07.25/12.0*100.0 , 05.0/12.0*100 , 037.5 )
+hexorng_dk = tohex( 07.50/12.0*100.0 , 03.5/12.0*100 , 060.0 )
+hexyllw_dk = tohex( 09.00/12.0*100.0 , 07.0/12.0*100 , 090.0 )
+hexgren_dk = tohex( 07.75/12.0*100.0 , 02.5/12.0*100 , 120.0 )
+hexaqua_dk = tohex( 07.75/12.0*100.0 , 02.0/12.0*100 , 180.0 )
+hexblue_dk = tohex( 08.00/12.0*100.0 , 03.0/12.0*100 , 262.5 )
+hexprpl_dk = tohex( 07.75/12.0*100.0 , 02.5/12.0*100 , 300.0 )
+hexpink_dk = tohex( 08.00/12.0*100.0 , 02.5/12.0*100 , 352.5 )
 
 print '# general'
 print 'Author:          nightsense'
